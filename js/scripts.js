e9ad2e000f81043a1599794b26bfcc733c6e0289
ed and jr <ed@>
2017-12-04 16:45:59 -0800

    add js

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,37 @@
+function test(word){
+  var char = word.charAt(0).toUpperCase();
+  var subWord =  word.substring(1);
+  return char.concat(subWord);
+}
 $(document).ready(function () {
-    $("form#checkList").submit(function (event) {
-        var toAdd = $("input[name=items]").val();
-        $(".listResult").append("<li class='item'>" + toAdd + "</li>");
+  var myInputIds = ['apple', 'egg','milk'];
+  var productsName = [];
+  var productsUpperCase = [];
+  var productsUpperCaseSort = [];
+    $("form#checkListForm").submit(function (event) {
         event.preventDefault();
-    });
+        myInputIds.forEach(function(InputId){
+        productsName.push($('#'+InputId).val());
+      });
+     productsUpperCase = productsName.map(function(word){
+       return test(word);
+     });
+     var productsUpperCaseSort = productsUpperCase.sort();
+      alert(productsUpperCaseSort);
+   });
+   productsUpperCaseSort.forEach(function(item){
+     $("#listResult").append("<li class='item'>" + item + "</li>");
+   });
+
 });
 
 
-
-
-
-
-
-//$(document).ready(function() {
-//  $("#checkList").submit(function() {
-//    var toAdd = $("input[name=checkListItem]").val();
-//    $(".list").append("<li class="item">" + toAdd + "</li>");
-//  });
+// var list = [];
+// list.push(toAdd);
+// event.preventDefault();
+// });
+// $("button").click(function(){
+// $(".listResult").append("<li class='item'>" + toAdd + "</li>");
+// $(".listResult").show();
+// });
 // });
